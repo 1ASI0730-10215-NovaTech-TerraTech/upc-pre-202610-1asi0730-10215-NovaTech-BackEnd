15479b58b901863bafe04e9e325d170e5fb9938b
GuillermoPromac <u202222275@upc.edu.pe>
2026-06-13 20:35:48 -0500

    feat(analytics_management): add ireportqueryservice and reportqueryservice for report data retrieval.

---
+++ NovaTech.TerraTech.Platform/AnalyticsManagement/Application/Internal/QueryServices/ReportQueryService.cs
@@ -0,0 +1,14 @@
+﻿using NovaTech.TerraTech.Platform.AnalyticsManagement.Application.Services;
+using NovaTech.TerraTech.Platform.AnalyticsManagement.Domain.Model.Aggregates;
+using NovaTech.TerraTech.Platform.AnalyticsManagement.Domain.Repositories;
+
+namespace NovaTech.TerraTech.Platform.AnalyticsManagement.Application.Internal.QueryServices;
+
+public class ReportQueryService(IReportRepository reportRepository) : IReportQueryService
+{
+    public async Task<Report?> GetReportByIdAsync(int reportId, CancellationToken cancellationToken = default) =>
+        await reportRepository.FindByIdAsync(reportId, cancellationToken);
+
+    public async Task<IEnumerable<Report>> GetAllReportsAsync(CancellationToken cancellationToken = default) =>
+        await reportRepository.ListAsync(cancellationToken);
+}

59457520a0270f562fd31eccc3f8276b74b2bcaf
GuillermoPromac <u202222275@upc.edu.pe>
2026-06-13 20:36:48 -0500

    feat(analytics_management): add assemblers for report creation and transformation.

---
+++ NovaTech.TerraTech.Platform/AnalyticsManagement/Interfaces/REST/Transform/CreateReportCommandFromResourceAssembler.cs
@@ -0,0 +1,20 @@
+﻿using NovaTech.TerraTech.Platform.AnalyticsManagement.Domain.Model.Commands;
+using NovaTech.TerraTech.Platform.AnalyticsManagement.Domain.Model.ValueObjects;
+using NovaTech.TerraTech.Platform.AnalyticsManagement.Interfaces.REST.Resources;
+
+namespace NovaTech.TerraTech.Platform.AnalyticsManagement.Interfaces.REST.Transform;
+
+public static class CreateReportCommandFromResourceAssembler
+{
+    public static CreateReportCommand ToCommandFromResource(CreateReportResource resource)
+    {
+        return new CreateReportCommand(
+            new DeviceId(resource.DeviceId),
+            new GeneratedAt(resource.GeneratedAt),
+            new MeanValue(resource.MeanValue),
+            new Variance(resource.Variance),
+            new StandardDeviation(resource.StandardDeviation),
+            new TechnicalInterpretation(resource.TechnicalInterpretation)
+        );
+    }
+}

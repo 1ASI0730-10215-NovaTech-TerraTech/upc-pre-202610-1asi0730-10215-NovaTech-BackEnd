cbc8f64369c0085b38bef75fccbbf3f580d228b4
GuillermoPromac <u202222275@upc.edu.pe>
2026-06-13 19:45:56 -0500

    feat(report_management): add createReportcommand record with necessary value objects.

---
+++ NovaTech.TerraTech.Platform/AnalyticsManagement/Domain/Model/Commands/CreateReportCommand.cs
@@ -0,0 +1,11 @@
+﻿using NovaTech.TerraTech.Platform.AnalyticsManagement.Domain.Model.ValueObjects;
+
+namespace NovaTech.TerraTech.Platform.AnalyticsManagement.Domain.Model.Commands;
+
+public record CreateReportCommand(
+    DeviceId DeviceId, 
+    GeneratedAt GeneratedAt, 
+    MeanValue MeanValue, 
+    Variance Variance, 
+    StandardDeviation StandardDeviation, 
+    TechnicalInterpretation TechnicalInterpretation );

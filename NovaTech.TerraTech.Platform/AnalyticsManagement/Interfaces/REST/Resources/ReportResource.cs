f36d92481855a8831c87c8a0dcf24810e11975ae
GuillermoPromac <u202222275@upc.edu.pe>
2026-06-13 20:36:20 -0500

    feat(analytics_management): add createreportresource and reportresource for report creation and representation.

---
+++ NovaTech.TerraTech.Platform/AnalyticsManagement/Interfaces/REST/Resources/ReportResource.cs
@@ -0,0 +1,13 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+
+namespace NovaTech.TerraTech.Platform.AnalyticsManagement.Interfaces.REST.Resources;
+
+[SwaggerSchema(Description = "A Report resource")]
+public record ReportResource(
+    [SwaggerParameter(Description = "Server-generated ID")] int Id,
+    [SwaggerParameter(Description = "Device ID")] int DeviceId,
+    [SwaggerParameter(Description = "Generation date")] DateTimeOffset GeneratedAt,
+    [SwaggerParameter(Description = "Mean value")] double MeanValue,
+    [SwaggerParameter(Description = "Variance")] double Variance,
+    [SwaggerParameter(Description = "Standard deviation")] double StandardDeviation,
+    [SwaggerParameter(Description = "Technical interpretation")] string TechnicalInterpretation);

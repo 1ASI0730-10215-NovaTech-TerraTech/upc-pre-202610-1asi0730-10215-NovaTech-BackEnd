627c7994b46d8898fdecab11609eb7713605f48b
breithner1911 <breithner1911@gmail.com>
2026-06-15 03:57:27 -0500

    feat(community_management): add CommunityProfileResource and CreateCommunityProfileResource for community profile handling.

---
+++ NovaTech.TerraTech.Platform/CommunityManagement/Interfaces/REST/Resources/CreateCommunityProfileResource.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using NovaTech.TerraTech.Platform.CommunityManagement.Domain.Model.ValueObjects;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace NovaTech.TerraTech.Platform.CommunityManagement.Interfaces.REST.Resources;
+
+[SwaggerSchema(Description = "Request payload to create a community profile")]
+public record CreateCommunityProfileResource(
+    [Required] [SwaggerParameter(Description = "Profile identifier")] string ProfileId,
+    [Required] [SwaggerParameter(Description = "Nickname")] string Nickname,
+    [Required] [SwaggerParameter(Description = "Reputation score")] int ReputationScore,
+    [Required] [SwaggerParameter(Description = "Public biography")] string PublicBio,
+    [Required] [SwaggerParameter(Description = "Visibility status")] VisibilityStatus VisibilityStatus
+);

366dd3cd4a4604a4fd2f31c4d19fb51853b77d57
GuillermoPromac <u202222275@upc.edu.pe>
2026-06-16 02:08:10 -0500

    feat(monitoring): implement device command service for creating and updating devices.

---
+++ NovaTech.TerraTech.Platform/Monitoring/Application/Services/IDeviceCommandService.cs
@@ -0,0 +1,11 @@
+﻿using NovaTech.TerraTech.Platform.Monitoring.Domain.Model.Aggregates;
+using NovaTech.TerraTech.Platform.Monitoring.Domain.Model.Commands;
+using NovaTech.TerraTech.Platform.Shared.Application.Model;
+
+namespace NovaTech.TerraTech.Platform.Monitoring.Application.Services;
+
+public interface IDeviceCommandService
+{
+    Task<Result<Device>> Handle(CreateDeviceCommand command, CancellationToken cancellationToken = default);
+    Task<Result<Device>> Handle(UpdateDeviceCommand command, CancellationToken cancellationToken = default);
+}

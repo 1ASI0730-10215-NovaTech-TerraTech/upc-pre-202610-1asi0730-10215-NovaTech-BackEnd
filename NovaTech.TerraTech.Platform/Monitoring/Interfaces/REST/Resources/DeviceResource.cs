364ecec42a74528c6b122b46200bff62903c3a89
GuillermoPromac <u202222275@upc.edu.pe>
2026-06-16 02:31:36 -0500

    feat(monitoring): add resource models for creating and updating devices,

---
+++ NovaTech.TerraTech.Platform/Monitoring/Interfaces/REST/Resources/DeviceResource.cs
@@ -0,0 +1,12 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+
+namespace NovaTech.TerraTech.Platform.Monitoring.Interfaces.REST.Resources;
+
+[SwaggerSchema(Description = "A Device resource")]
+public record DeviceResource(
+    [SwaggerParameter(Description = "Server-generated ID")] int Id,
+    [SwaggerParameter(Description = "Field ID")] int FieldId,
+    [SwaggerParameter(Description = "MAC address")] string MacAddress,
+    [SwaggerParameter(Description = "Device status")] string Status,
+    [SwaggerParameter(Description = "Last synchronization")] DateTimeOffset LastSync
+);
---
+++ NovaTech.TerraTech.Platform/Monitoring/Interfaces/REST/Resources/UpdateDeviceResource.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace NovaTech.TerraTech.Platform.Monitoring.Interfaces.REST.Resources;
+
+[SwaggerSchema(Description = "Request payload to update a Device")]
+public record UpdateDeviceResource(
+    [Required]
+    [RegularExpression(@"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$", ErrorMessage = "MAC address must be in format XX:XX:XX:XX:XX:XX or XX-XX-XX-XX-XX-XX")]
+    [SwaggerParameter(Description = "MAC address")] string MacAddress,
+    
+    [Required]
+    [SwaggerParameter(Description = "Device status (ONLINE, OFFLINE, LOW_BATTERY)")] string Status,
+    
+    [Required]
+    [SwaggerParameter(Description = "Last synchronization timestamp (ISO format)")] DateTimeOffset LastSync
+);

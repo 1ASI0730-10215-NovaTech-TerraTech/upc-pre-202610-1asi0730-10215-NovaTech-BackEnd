dc3bf73f1e1fd54503f9012a379dc1348e3882d1
GuillermoPromac <u202222275@upc.edu.pe>
2026-06-16 02:04:30 -0500

    feat(monitoring): implement device aggregate with creation and update functionality.

---
+++ NovaTech.TerraTech.Platform/Monitoring/Domain/Model/Aggregates/Device.cs
@@ -0,0 +1,38 @@
+﻿using NovaTech.TerraTech.Platform.Monitoring.Domain.Model.ValueObjects;
+using NovaTech.TerraTech.Platform.Monitoring.Domain.Model.Commands;
+
+namespace NovaTech.TerraTech.Platform.Monitoring.Domain.Model.Aggregates;
+
+public partial class Device
+{
+    protected Device()
+    {
+        FieldId = null!;
+        MacAddress = null!;
+        Status = null!;
+        LastSync = null!;
+    }
+
+    public Device(CreateDeviceCommand command)
+    {
+        ArgumentNullException.ThrowIfNull(command);
+        
+        FieldId = command.FieldId;
+        MacAddress = command.MacAddress;
+        Status = command.Status;
+        LastSync = new LastSync(command.LastSync);
+    }
+    
+    public int Id { get; private set; }
+    public FieldId FieldId { get; private set; }
+    public MacAddress MacAddress { get; private set; }
+    public DeviceStatus Status { get; private set; }
+    public LastSync LastSync { get; private set; }
+    
+    public void Update(UpdateDeviceCommand command)
+    {
+        MacAddress = command.MacAddress;
+        Status = command.Status;
+        LastSync = new LastSync(command.LastSync);
+    }
+}

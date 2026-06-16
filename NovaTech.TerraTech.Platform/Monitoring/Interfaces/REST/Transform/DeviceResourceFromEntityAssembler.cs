ebf56320c289f43ae550fc49127df18739846a65
GuillermoPromac <u202222275@upc.edu.pe>
2026-06-16 02:31:55 -0500

    feat(monitoring): add assemblers for device command and resource transformations.

---
+++ NovaTech.TerraTech.Platform/Monitoring/Interfaces/REST/Transform/DeviceResourceFromEntityAssembler.cs
@@ -0,0 +1,16 @@
+﻿using NovaTech.TerraTech.Platform.Monitoring.Domain.Model.Aggregates;
+using NovaTech.TerraTech.Platform.Monitoring.Interfaces.REST.Resources;
+
+namespace NovaTech.TerraTech.Platform.Monitoring.Interfaces.REST.Transform;
+
+public static class DeviceResourceFromEntityAssembler
+{
+    public static DeviceResource ToResourceFromEntity(Device entity) =>
+        new(
+            entity.Id,
+            entity.FieldId.Value,
+            entity.MacAddress.Value,
+            entity.Status.Value,
+            entity.LastSync.Value
+        );
+}
---
+++ NovaTech.TerraTech.Platform/Monitoring/Interfaces/REST/Transform/UpdateDeviceCommandFromResourceAssembler.cs
@@ -0,0 +1,18 @@
+﻿using NovaTech.TerraTech.Platform.Monitoring.Domain.Model.Commands;
+using NovaTech.TerraTech.Platform.Monitoring.Domain.Model.ValueObjects;
+using NovaTech.TerraTech.Platform.Monitoring.Interfaces.REST.Resources;
+
+namespace NovaTech.TerraTech.Platform.Monitoring.Interfaces.REST.Transform;
+
+public static class UpdateDeviceCommandFromResourceAssembler
+{
+    public static UpdateDeviceCommand ToCommandFromResource(int id, UpdateDeviceResource resource)
+    {
+        return new UpdateDeviceCommand(
+            id,
+            new MacAddress(resource.MacAddress),
+            DeviceStatus.Create(resource.Status),
+            resource.LastSync
+        );
+    }
+}

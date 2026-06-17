48905e418eb64cff96582f397f10aa828d0be2c9
GuillermoPromac <u202222275@upc.edu.pe>
2026-06-16 23:46:11 -0500

    feat(monitoring): add query to retrieve fields by soil type.

---
+++ NovaTech.TerraTech.Platform/Monitoring/Domain/Model/Queries/GetFieldBySoilTypeQuery.cs
@@ -0,0 +1,9 @@
+﻿using NovaTech.TerraTech.Platform.Monitoring.Domain.Model.ValueObjects;
+
+namespace NovaTech.TerraTech.Platform.Monitoring.Domain.Model.Queries;
+
+/// <summary>
+/// Query to get fields filtered by their soil type.
+/// </summary>
+/// <param name="SoilType">The type of soil (e.g., Sandy, Clay-loam, etc.).</param>
+public record GetFieldBySoilTypeQuery(SoilType SoilType);

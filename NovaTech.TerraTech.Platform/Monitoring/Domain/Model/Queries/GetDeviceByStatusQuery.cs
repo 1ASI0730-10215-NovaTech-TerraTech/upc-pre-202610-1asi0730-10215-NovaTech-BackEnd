99852be1e734a8a1fbac5080aec01356584c6eba
GuillermoPromac <u202222275@upc.edu.pe>
2026-06-16 19:23:28 -0500

    feat(monitoring): add query to retrieve devices by their status.

---
+++ NovaTech.TerraTech.Platform/Monitoring/Domain/Model/Queries/GetDeviceByStatusQuery.cs
@@ -0,0 +1,9 @@
+﻿using NovaTech.TerraTech.Platform.Monitoring.Domain.Model.ValueObjects;
+
+namespace NovaTech.TerraTech.Platform.Monitoring.Domain.Model.Queries;
+
+/// <summary>
+/// Query to get all devices filtered by their status.
+/// </summary>
+/// <param name="Status">The device status to filter by (ONLINE, OFFLINE, LOW_BATTERY).</param>
+public record GetDevicesByStatusQuery(DeviceStatus Status);

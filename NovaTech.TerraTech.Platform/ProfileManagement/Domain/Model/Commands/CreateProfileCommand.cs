28ef8e074bcaead3590280db3ee0c0d1e4783996
breithner1911 <breithner1911@gmail.com>
2026-06-22 22:57:06 -0500

    feat(profile_management): change userid type from string to int in createprofilecommand.

--- NovaTech.TerraTech.Platform/ProfileManagement/Domain/Model/Commands/CreateProfileCommand.cs
+++ NovaTech.TerraTech.Platform/ProfileManagement/Domain/Model/Commands/CreateProfileCommand.cs
@@ -1,7 +1,7 @@
 namespace NovaTech.TerraTech.Platform.ProfileManagement.Domain.Model.Commands;
 
 public record CreateProfileCommand(
-    string UserId,
+    int UserId,
     string FundoName,
     string ContactPhone,
     double MoistureThreshold,

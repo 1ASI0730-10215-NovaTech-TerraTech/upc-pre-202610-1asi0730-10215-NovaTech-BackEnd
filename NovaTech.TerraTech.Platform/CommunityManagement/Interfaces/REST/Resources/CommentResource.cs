f2bb4d1a5fc5659f62db02f1be42f4613fe2d62f
breithner1911 <breithner1911@gmail.com>
2026-07-02 02:50:28 -0500

    feat(comment_management): change AuthorProfileId and TargetProfileId types from string to int in CommentResource and CreateCommentResource.

--- NovaTech.TerraTech.Platform/CommunityManagement/Interfaces/REST/Resources/CommentResource.cs
+++ NovaTech.TerraTech.Platform/CommunityManagement/Interfaces/REST/Resources/CommentResource.cs
@@ -5,8 +5,8 @@ namespace NovaTech.TerraTech.Platform.CommunityManagement.Interfaces.REST.Resour
 [SwaggerSchema(Description = "Comment resource")]
 public record CommentResource(
     [SwaggerParameter(Description = "Comment database identifier")] int Id,
-    [SwaggerParameter(Description = "Author profile identifier")] string AuthorProfileId,
-    [SwaggerParameter(Description = "Target profile identifier")] string TargetProfileId,
+    [SwaggerParameter(Description = "Author profile identifier")] int AuthorProfileId,
+    [SwaggerParameter(Description = "Target profile identifier")] int TargetProfileId,
     [SwaggerParameter(Description = "Content of the comment")] string Content,
     [SwaggerParameter(Description = "Rating score (0-5)")] int Rating,
     [SwaggerParameter(Description = "Creation timestamp")] DateTimeOffset? CreatedAt
